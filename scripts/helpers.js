a072acf31721aa7548edd7d7571e74c80ce6b301
chris.miranda <christophermiranda@pursuit.org>
2023-11-04 21:04:26 -0400

    added more required fields

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,10 +1,10 @@
-function generateNewMovie (title, director, release, cover, stock){
-    const li = movieTemplate(title, director, release, cover, stock)
+function generateNewMovie (title, director, release, cover, stock, genre, rating, price){
+    const li = movieTemplate(title, director, release, cover, stock, genre, rating, price)
     const ul = document.querySelector('ul')
     ul.append(li)
 }
 
-function movieTemplate(title, director, release, cover, stock){
+function movieTemplate(title, director, release, cover, stock, genre, rating, price){
     const li = document.createElement('li')
     li.classList.add("single-movie")
 
@@ -14,11 +14,14 @@ function movieTemplate(title, director, release, cover, stock){
         event.target.closest('.single-movie').remove()
     })
 
-    if (title && director && release && cover && stock){
+    if (title && director && release && cover && stock && genre && rating && price){
         li.innerHTML = `<h3><img src="${cover}" width="200" alt="${title} Movie Cover"></h3>
         <p><strong>Movie Title: </strong>${title}</p>
         <p><strong>Directed By: </strong>${director}</p>
         <p><strong>Release Date: </strong> ${release}</p>
+        <p><strong>Genre: </strong> ${genre} </p>
+        <p><strong>IMDB Rating: </strong> ${rating}⭐️/10⭐️</p>
+        <p><strong>Price: </strong> $${price}</p>
         <button class="stock-value">${stock}</button>`
         li.append(removeButton)
     }
